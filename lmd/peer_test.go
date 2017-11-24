0022e4e2acd8d05586ac698ce0146bbde166ebe0
Sven Nierlein <sven@nierlein.de>
2017-11-24 14:02:07 +0100

    add result parser test

--- lmd/peer_test.go
+++ lmd/peer_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"sync"
 	"testing"
 )
@@ -44,3 +46,62 @@ func TestPeerHTTPComplete(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestParseResultJSON(t *testing.T) {
+
+	waitGroup := &sync.WaitGroup{}
+	shutdownChannel := make(chan bool)
+	connection := Connection{Name: "Test", Source: []string{"http://localhost/test/", "http://clusternode/test"}}
+	peer := NewPeer(&Config{}, &connection, waitGroup, shutdownChannel)
+
+	req, _, err := NewRequest(bufio.NewReader(bytes.NewBufferString("GET services\nColumns: host_name description\nOutputFormat: json\n")))
+	if err != nil {
+		panic(err.Error())
+	}
+	data := []byte(`[
+	 ["host1", "desc1"],
+	 ["host2", "desc2"]
+	]`)
+
+	res, err := peer.parseResult(req, &data)
+
+	if err := assertEq(2, len(res)); err != nil {
+		t.Fatal(err)
+	}
+	if err := assertEq(2, len(res[0])); err != nil {
+		t.Error(err)
+	}
+	if err := assertEq("host2", res[1][0]); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestParseResultWrappedJSON(t *testing.T) {
+
+	waitGroup := &sync.WaitGroup{}
+	shutdownChannel := make(chan bool)
+	connection := Connection{Name: "Test", Source: []string{"http://localhost/test/", "http://clusternode/test"}}
+	peer := NewPeer(&Config{}, &connection, waitGroup, shutdownChannel)
+
+	req, _, err := NewRequest(bufio.NewReader(bytes.NewBufferString("GET services\nColumns: host_name description\nOutputFormat: wrapped_json\n")))
+	if err != nil {
+		panic(err.Error())
+	}
+	data := []byte(`{"data": [
+	 ["host1", "desc1"],
+	 ["host2", "desc2"]
+	]}`)
+
+	res, err := peer.parseResult(req, &data)
+
+	if err := assertEq(2, len(res)); err != nil {
+		t.Fatal(err)
+	}
+	if err := assertEq(2, len(res[0])); err != nil {
+		t.Error(err)
+	}
+	if err := assertEq("host2", res[1][0]); err != nil {
+		t.Error(err)
+	}
+}
+
