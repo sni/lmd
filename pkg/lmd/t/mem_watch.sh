5a086b69253d046841f7e77549915c1e166eeace
Ahmet Oeztuerk <Ahmet.Oeztuerk@consol.de>
2026-04-17 13:46:58 +0200

    test: add mb output to the mem_watch script

--- pkg/lmd/t/mem_watch.sh
+++ pkg/lmd/t/mem_watch.sh
@@ -13,19 +13,21 @@ while true; do
 
     if [ -n "$PID" ]; then
         echo "--- Process started (PID: $PID) ---"
-        CURRENT_PEAK=0
+        CURRENT_PEAK_KB=0
 
         # 2. Monitor while the process is alive
         while kill -0 "$PID" 2>/dev/null; do
             # Get RSS memory in KB
             MEM_KB=$(ps -o rss= -p "$PID" | tr -d ' ')
+            MEM_MB=$(( ${MEM_KB} / 1024 ))
 
             if [ -n "$MEM_KB" ]; then
                 # Update peak
-                if [ "$MEM_KB" -gt "$CURRENT_PEAK" ]; then
-                    CURRENT_PEAK=$MEM_KB
+                if [ "$MEM_KB" -gt "$CURRENT_PEAK_KB" ]; then
+                    CURRENT_PEAK_KB=$MEM_KB
+                    CURRENT_PEAK_MB=$(( $CURRENT_PEAK_KB / 1024 ))
                 fi
-                echo "Current: ${MEM_KB} KB | Peak: ${CURRENT_PEAK} KB"
+                echo "Current: ${MEM_KB} KB | Peak: ${CURRENT_PEAK_KB} KB , ${CURRENT_PEAK_MB} MB"
             fi
 
             sleep "$INTERVAL"
@@ -33,7 +35,7 @@ while true; do
 
         # 3. Report peak on termination
         echo "--- Process terminated ---"
-        echo "Final Peak Consumption: ${CURRENT_PEAK} KB"
+        echo "Final Peak Consumption: ${CURRENT_PEAK_KB} KB , ${CURRENT_PEAK_MB} MB"
         echo "Waiting for next execution..."
         echo "--------------------------"
     fi
